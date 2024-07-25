d3d3426424cb0fa9af23af795e48802e965b5e50
Gerlin Gonzalez <gegovaceo@gmail.com>
2024-07-25 15:03:56 -0500

    First Commit

--- trans/settings.py
+++ trans/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6o-fotlr=@@k_&lthz3it_u%n&^la2&a_l749jq02=w-ddy5wc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
