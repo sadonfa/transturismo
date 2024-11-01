6f1488b23e063fcd3acadc4b2e268cfb9e4ef3bb
Gerlin Gonzalez <sadonfa@gmail.com>
2024-11-01 09:58:12 -0500

    update ofertas

--- core/views.py
+++ core/views.py
@@ -4,11 +4,18 @@ from tours.models import Tours, Cena
 # Create your views here.
 
 def home(request):
-    lugares = Cena.objects.all()[0:3]
+    lugares = Tours.objects.filter(title='Tierra Bomba')
     tours = Tours.objects.all()[0:6]
+    ofertas = [
+        Tours.objects.filter(title='Isla del encanto'),
+        Tours.objects.filter(title='CITY TOUR HISTORICO'),
+        Tours.objects.filter(title='TOUR BARÚ TERRESTRE')
+    ]
+
 
     return render(request, "home.html",{
         'title': 'home',
         'tours': tours,
-        'lugares': lugares
+        'lugares': lugares,
+        'ofertas': ofertas
     })
