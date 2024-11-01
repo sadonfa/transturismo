d02cbeb1b67d666ac5e59f7351269f6114acf7a3
Gerlin Gonzalez <sadonfa@gmail.com>
2024-11-01 12:05:11 -0500

    update info

--- core/views.py
+++ core/views.py
@@ -7,7 +7,7 @@ def home(request):
     lugares = Tours.objects.filter(title='Tierra Bomba')
     tours = Tours.objects.all()[0:6]
     ofertas = [
-        Tours.objects.filter(title='Isla del encanto'),
+        Tours.objects.filter(title='Isla del encanto - pasadia premium'),
         Tours.objects.filter(title='CITY TOUR HISTORICO'),
         Tours.objects.filter(title='TOUR BARÚ TERRESTRE')
     ]
