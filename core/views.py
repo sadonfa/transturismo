079cb29bad5d067b7c6c63edb5e05022b5285224
Gerlin Gonzalez <sadonfa@gmail.com>
2024-12-05 12:24:27 -0500

    Menu

--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from tours.models import Tours, Cena
+import requests
 
 # Create your views here.
 
@@ -18,4 +19,19 @@ def home(request):
         'tours': tours,
         'lugares': lugares,
         'ofertas': ofertas
+    })
+
+def test(request):
+    # response = requests.get('https://jsonplaceholder.typicode.com/posts')     
+    # response = requests.get('https://mybusiness.googleapis.com/v4/{parent=accounts/*/locations/*}/reviews')
+    response = requests.get('https://mybusiness.googleapis.com/v4/accounts/345438785308-9v1gu6pils5qjdqlrqnn1ihb2up8n94o.apps.googleusercontent.com/locations/{locationId}/reviews') 
+
+    if response.status_code == 200:  
+        data = response.json()  
+    else:  
+        data="Error retrieving data"
+
+    return render(request, "test.html", {
+        'title': 'test',
+        'data': data
     })
